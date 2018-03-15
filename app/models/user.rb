e82f7b9bbb63cbc75675005713c37a8231a9cd5a
毛 若愚 <mao.ruoyu@mf-0414-mm01.mfw.local>
2018-03-15 15:30:14 +0900

    something about reset password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save { email.downcase! }
   validates :name, presence: true, length:{ maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  #get 'password_resets/new'
+
+  #get 'password_resets/edit'
+
   #get 'sessions/new'
 
   #get 'users/new'
@@ -23,4 +27,6 @@ Rails.application.routes.draw do
 
 
   resources :users
+  resources :account_activations, only: [:edit]
+  resources :password_resets, only:[:new, :create, :edit, :update]
 end
