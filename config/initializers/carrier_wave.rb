136cc1b0984e438e68c5caaa4f8b84515652a05c
毛 若愚 <mao.ruoyu@mf-0414-mm01.mfw.local>
2018-03-20 11:37:05 +0900

    change credential to credentials

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
    CarrierWave.configure do |config|
    	config.fog_provider = "fog/aws"
-  	 config.fog_credential = {
+  	 config.fog_credentials = {
   		# Configuration for Amazon S3
   		:provider => 'AWS',
   		:aws_access_key_id => ENV['S3_ACCESS_KEY'],
